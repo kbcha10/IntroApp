21b044a56a9c70d27d0e5b98322f3158c2a38455
kbcha10 <herbists.0v0@gmail.com>
2019-05-20 02:35:35 +0900

    質問が追加できるぞ！

--- IntroApp/ViewController/QuestionChoiceController.swift
+++ IntroApp/ViewController/QuestionChoiceController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import RealmSwift
 
-class QuestionChoiceController: UITableViewController {
+
+class QuestionChoiceController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     let realm = try! Realm()
@@ -18,25 +19,40 @@ class QuestionChoiceController: UITableViewController {
     var QuestionArray:Results<QuestionModel>!
     var questionNum: [Int] = []
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //IntroモデルとQuestionモデルを読み込み
         QuestionArray = realm.objects(QuestionModel.self)
-        
         tableView.register(UINib(nibName: "QuestionCell", bundle: nil),forCellReuseIdentifier: "questionCell")
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    // セクションごとにデータ要素数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return QuestionArray.count
+    }
+    
+    // セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
         questionNum=[]
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return QuestionArray.count
+    /*// セクションヘッダ
+     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?  {
+     return sectionName[section]
+     }*/
+    
+    // セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // セル生成
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath) as! QuestionCell
         cell.questionLabel!.text = QuestionArray[indexPath.row].question
         if (cell.isChecked){
@@ -45,11 +61,14 @@ class QuestionChoiceController: UITableViewController {
         print(questionNum)
         return cell
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    // セクションヘッダの高さ
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //セル選択時の挙動
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at:indexPath)
         
         if cell?.accessoryType != .checkmark {
@@ -58,11 +77,50 @@ class QuestionChoiceController: UITableViewController {
         } else {
             cell?.accessoryType = .none
         }
-        /*let vc = storyboard!.instantiateViewController(withIdentifier:"NextViewController") as! NextViewController
-        vc.quizArray = questionNum*/
-        
     }
     
+    @IBAction func displayAlert() {
+        
+        let alert = UIAlertController(title: "質問を追加", message: "追加したい質問を入力してください", preferredStyle: .alert)
+        let cancelButton = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler: nil)
+        let okayButton = UIAlertAction(title: "追加", style: UIAlertAction.Style.default, handler: {[weak alert] (action) -> Void in
+            guard let textFields = alert?.textFields else {
+                return
+            }
+            
+            guard !textFields.isEmpty else {
+                return
+            }
+            
+            //OKボタンを押した時に質問をRealmに保存
+            for text in textFields {
+                let QuestionCount = self.QuestionArray.count
+                let question = QuestionModel()
+                question.question = text.text!
+                question.id=QuestionCount
+                try! self.realm.write {
+                    self.realm.add(question)
+                }
+                //テーブルに反映
+                self.tableView.reloadData()
+            }
+        })
+        alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+            
+        })
+        
+        alert.addAction(okayButton)
+        alert.addAction(cancelButton)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
     
 
-}
+    
+
+    
+
+
+
+
